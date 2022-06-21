d99ed70fcf7b4d511bb5459bf54449c9606d7a92
amir <galimzyanoffam@yandex.ru>
2022-06-21 13:24:23 +0300

    install commit

---
+++ resources/views/admin/home/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.admin_layouts');
+
+@section('title', 'Главная');
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Главная</h1>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <!--F small box -->
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>{{ $productsts_count }}</h3>
+
+                            <p>Количевство книг</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-bag"></i>
+                        </div>
+                        <a href="{{route('product.index')}}" class="small-box-footer">Подробнее<i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+@endsection
